72abc4b9b20e9c1079c1002e1c6941d33301042d
King9587 <55016272+King9587@users.noreply.github.com>
2022-01-29 12:35:07 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
+// 设置图片切换
 let myImage = document.querySelector('img');
+
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.jpg') {
-    myImage.setAttribute('src', 'images/firefox-icon.png');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute('src', 'images/firefox-icon.jpg');
+    myImage.setAttribute ('src','images/firefox-icon.png');
   }
 }
+
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 function setUserName() {
